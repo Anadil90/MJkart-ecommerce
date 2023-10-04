03637b0a89799f2be1792cc09ec1782182bf0e48
Jsse Moyer <jedddon@gmail.com>
2023-10-04 15:25:10 -0400

    Pushed Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,46 @@
-import { Container, Col, Row } from 'reactstrap';
-import './home.css'
+import { Container, Col, Row } from "reactstrap";
+import "./home.css";
 
 const Home = () => {
   return (
     <Container>
-      This is going to be a component showing 4 information only divs telling the user of
-      the advantages of shopping online from MJkart.
+      This is going to be a component showing 4 information only divs telling
+      the user of the advantages of shopping online from MJkart.
       <Row>
-        <Col md='3'>
-          <div className='shopper-info'>
-            <p className='info-text'>
-              Reliable online shopping
-            </p>
-          
+        <Col md="3">
+          <div className="shopper-info">
+            <p className="info-text">Reliable online shopping</p>
           </div>
         </Col>
 
-        <Col md='3'>
-          <div className='shopper-info'>
-            <p className='info-text'>
+        <Col md="3">
+          <div className="shopper-info">
+            <p className="info-text">
               Super customer support team ready to answer your inquiries
             </p>
-          
           </div>
         </Col>
 
-        <Col md='3'>
-          <div className='shopper-info'>
-            <p className='info-text'>
-              Fast delivery with an internal package tracking feature to know where your ordered products are.
+        <Col md="3">
+          <div className="shopper-info">
+            <p className="info-text">
+              Fast delivery with an internal package tracking feature to know
+              where your ordered products are.
             </p>
           </div>
         </Col>
 
-        <Col md='3'>
-          <div className='shopper-info'>
-            <p className='info-text'>
-              No bogus products! We only list products that will gain your satisfaction
+        <Col md="3">
+          <div className="shopper-info">
+            <p className="info-text">
+              No bogus products! We only list products that will gain your
+              satisfaction
             </p>
           </div>
         </Col>
-      
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
