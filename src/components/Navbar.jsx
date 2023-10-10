e3e924d754891f07f1a30328ebdf4e6d464ec998
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-10-11 00:55:25 +0200

    Small changes
    Cart logo changed, login modal implemented.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,36 @@
 import "../App.css";
 import "./navbar.css";
 import { 
-  Navbar, Nav, NavbarBrand, Button, NavItem, NavLink, Modal, ModalBody, ModalHeader
+  Navbar, Nav, NavbarBrand, Button, NavItem, NavLink, Modal, ModalBody, ModalHeader, FormGroup, Label
 } from "reactstrap";
 import MJKARTLOGO from "../assets/racetosavings.png";
-import UserLog from "../assets/3541871.png";
+import userIcon from "../assets/user-icon.png";
 import { useState } from "react";
+import cart from "../assets/add-to-cart.png";
+import { Formik, Field, Form } from 'formik';
 
 const NavBar = () => {
 
-const [online, setOnline] = useState("user offline"); //initialise default state of user
-const [modalOpen, setModalOpen] = useState(false);
-//const [toggleModal, setModalToggle] = useState(false);
-  //const [username, setUsername ] = useState('');
+const [online, setOnline] = useState("user offline"); //user state
+const [userModal, setUserModal] = useState(false)
+const [username, setUsername ] = useState('');
+const [password, setPassword] = useState(undefined);
+const [cartLoaded, setCartLoaded] = useState(false);//shopping cart state
+
+const user = {
+  username: '',
+  password: ''
+}
+console.log(user)
+const handleLogin = (values) => {
+  user.username = values.username;
+  user.password = values.password;
+  setUserModal(false)
+}
+
+const loadCart = () => {
+  setCartLoaded(true);
+}
   
   return (
     <div>
@@ -23,35 +41,53 @@ const [modalOpen, setModalOpen] = useState(false);
         </NavbarBrand>
 
         <Nav>
-          <NavItem>
-            <NavLink href="/">
-          <img className="navbarStyling" alt="logo-created" />
-        </NavLink>
-          </NavItem>
-        
 
         <NavItem>
-          
-          <Button onClick={() => setModalOpen(true)}></Button>
-<i src={"../assets/add-to-cart.png"}></i>
-          <Modal isOpen={modalOpen}>
-            <ModalHeader toggle={() => {setModalOpen(false)}}>Shopping Cart</ModalHeader>
-            <ModalBody>
-              <p>This is your shopping cart content.</p>
-            </ModalBody>
-          </Modal>
+          <img src={cart} className="shopping-cart" onClick={() => loadCart()}/>
+          <Modal isOpen={cartLoaded}>
+        <ModalHeader toggle={() => {setCartLoaded(false)}}>Shopping Cart</ModalHeader>
+        <ModalBody>
+            <p>This is your shopping cart content.</p>
+        </ModalBody>
+        </Modal>
 
-          {/*<Button
+          <Button
             onClick={() => {
-                setOnline("user online 🟢");
+                setUserModal(true);
+                {/*setOnline("user online 🟢");*/}
             }}
             style={{ color: "grey", fontWeight: 100, margin: 15, color: "#fff" }}
             className="authentication"
           >
             Sign In
           </Button>
+
+          <Modal isOpen={userModal}>
+            <ModalHeader toggle={() => {setUserModal(false)}}>Login</ModalHeader>
+            <ModalBody>
+              <Formik
+              initialValues={{username: '', password: ''}}
+              onSubmit={handleLogin}
+              >
+                <Form>
+                  <FormGroup>
+                    <Label htmlFor='username' className='form-control'>Username</Label>
+                    <Field id='username' name='username' placeholder='Username' className='form-control'/>
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Label htmlFor='password' className='form-control'>Password</Label>
+                    <Field id='password' name='password' placeholder='Password' className='form-control'/>
+                  </FormGroup>
+                  <Button type='submit' color='primary'>Login</Button>
+                </Form>
+              </Formik>
+            </ModalBody>
+          </Modal>
+
           
-          <Button
+          
+          {/*<Button
         
             onClick={() => {
                 setOnline(!online);
@@ -62,8 +98,6 @@ const [modalOpen, setModalOpen] = useState(false);
             Sign out
           </Button>
           */}
-
-          <img className='user-logo' src={UserLog} alt="logo-created" />
           </NavItem>
       
         </Nav>
